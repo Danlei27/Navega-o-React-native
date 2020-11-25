a718f97c8808f1c82c2e62a33ec38f6aacbee977
danlei santos <danleisantos@hotmails.com>
2020-11-25 19:28:19 -0300

    Rotas, componentes e navegação

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { SafeAreaView, Text} from 'react-native'
-
+import { SafeAreaView, Text } from 'react-native'
+import TelaA from './views/TelaA'
+import TelaB from './views/TelaB'
+import TelaC from './views/TelaC'
 export default props => {
     return(
-        <SafeAreaView>
-            <Text>App</Text>
+        <SafeAreaView style={{flex: 1}}>
+            <TelaA/>
+            <TelaB/>
+            <TelaC/>
         </SafeAreaView>
     )
 }
